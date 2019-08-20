a15e266dddc1f24493340e72b06835af7a5c7b16
LuigiDerson <luigi.prodesign@gmail.com>
2019-08-21 01:26:11 +0200

    Notes-app

---
+++ old/notes-app.js
@@ -0,0 +1,66 @@
+/* const notes = [{
+  title: "my next trip",
+  body: "I would like to go to Spain"
+}, {
+  title: "Habbits to work on",
+  body: "Exercise. Eating a bit better"
+}, {
+  title: "Office modification",
+  body: "Get new seat"
+}] */
+
+let notes = [];
+
+const filters = {
+  searchText: ""
+}
+
+// Check for existing saved data
+const notesJSON = localStorage.getItem("notes")
+
+if (notesJSON !== null) {
+  notes = JSON.parse(notesJSON)
+}
+
+
+const renderNotes = (notes, filters) => {
+  const filteredNotes = notes.filter(note =>
+    note.title.toLowerCase().includes(filters.searchText.toLowerCase()));
+
+    document.querySelector("#notes").innerHTML = "";
+
+    filteredNotes.forEach(function(note) {
+      const noteEl = document.createElement("p");
+
+      if (note.title.length > 0) {
+        noteEl.textContent = note.title;
+      } else {
+        noteEl.textContent = "unnamed note";
+      }
+      document.querySelector("#notes").appendChild(noteEl);
+    })
+}
+
+// First call of the function
+renderNotes(notes, filters);
+
+document.querySelector("#create-note").addEventListener("click", function(e) {
+  notes.push({
+    title: "",
+    body: ""
+  })
+  localStorage.setItem("notes", JSON.stringify(notes))
+  renderNotes(notes, filters)
+})
+
+
+document.querySelector("#search-text").addEventListener("input", function(e) {
+  filters.searchText = e.target.value;
+
+  // Second call of the function
+  renderNotes(notes, filters);
+})
+
+document.querySelector("#filter-by").addEventListener("change", function(evt) {
+  console.log(evt.target.value);
+})
